7c0426cb3a59ab76c3735ff5c17dd0f2e2b48a26
Eugen Ciur <eugen@papermerge.com>
2021-09-06 07:00:31 +0200

    pretty good s3 client

--- papermerge/storage/s3.py
+++ papermerge/storage/s3.py
@@ -1,3 +1,4 @@
+import os
 import boto3
 import botocore
 
@@ -9,8 +10,27 @@ class S3Error(botocore.exceptions.ClientError):
 class S3:
 
     def __init__(self):
-        self._client = boto3.client('s3')
-        self._resource = boto3.resource('s3')
+        self._aws_access_key_id = os.environ.get(
+            "S3_ACCESS_KEY_ID", None
+        )
+        self._aws_secret_access_key = os.environ.get(
+            "S3_SECRET_KEY", None
+        )
+        self._endpoint_url = os.environ.get(
+            "S3_ENDPOINT_URL", None
+        )
+        self._client = boto3.client(
+            's3',
+            aws_access_key_id=self._aws_access_key_id,
+            aws_secret_access_key=self._aws_secret_access_key,
+            endpoint_url=self._endpoint_url
+        )
+        self._resource = boto3.resource(
+            's3',
+            aws_access_key_id=self._aws_access_key_id,
+            aws_secret_access_key=self._aws_secret_access_key,
+            endpoint_url=self._endpoint_url
+        )
 
     @property
     def client(self):
